cf8153145d3b02415f4cae92ed795de4f5cfc7b5
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-21 08:14:58 +0530

    refractor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { WalletDbService } from './prisma-db/wallets/wallet.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly walletService: WalletDbService,
-  ) {}
+  constructor(private readonly walletService: WalletDbService) {}
 
-  @Post("/wallet")
+  @Post('/wallet')
   async addWallet() {
     return this.walletService.addWallet('1', true);
   }
 
-  @Get("/wallet")
+  @Get('/wallet')
   async getWallet() {
     return this.walletService.getWallet('1');
   }
-
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaDbModule } from './prisma-db/prisma-db.module';
-import { WalletDbService } from './prisma-db/wallets/wallet.service';
 import { ScheduleModule } from '@nestjs/schedule';
 @Module({
   imports: [PrismaDbModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, WalletDbService],
 })
 export class AppModule {}

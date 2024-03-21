85dd53a59d86a8c1b932180605fd1b4d4038b8d4
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-21 18:44:14 +0530

    feat: create cron handler

--- src/wallet-alerts/alert-services/cron.service.ts
+++ src/wallet-alerts/alert-services/cron.service.ts
@@ -2,12 +2,15 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { WalletDbService } from 'src/prisma-db/wallets/wallet.service';
 import { FetchService } from '../simple-hash-services/fetch.service';
+import { DeltaService } from './delta.service';
+import { Wallet } from '@prisma/client';
 
 @Injectable()
 export class CronService {
   constructor(
     private readonly walletDbActions: WalletDbService,
     private readonly fetchService: FetchService,
+    private readonly deltaService: DeltaService,
   ) {}
 
   private readonly logger = new Logger(CronService.name);
@@ -25,17 +28,20 @@ export class CronService {
     const walletIds = wallets.map((wallet) => wallet.wId);
     //call simple hash API to fetch wallet activity
     try {
-      const walletsLatestTxnData: Record<
-        string,
-        {
-          collectionId: string;
-          transactionId: string;
-          From: string;
-          To: string;
-          TimeStamp: string;
-        }
-      > = await this.fetchService.fetchWalletsLatestTxn(walletIds);
-      console.log('walletsLatestTxnData: ', walletsLatestTxnData);
+      const {
+        walletsLatestTxnData,
+      }: {
+        walletsLatestTxnData: Record<
+          string,
+          {
+            collectionId: string;
+            transactionId: string;
+            From: string;
+            To: string;
+            TimeStamp: string;
+          }
+        >;
+      } = await this.fetchService.fetchWalletsLatestTxn(walletIds);
 
       //update wallets with activity
       await this.walletDbActions.updateWalletsFields(
@@ -53,5 +59,41 @@ export class CronService {
   @Cron(CronExpression.EVERY_SECOND)
   async handleCron() {
     this.logger.log('CRON Alerts');
+    //get all wallets that require alerting
+    const wallets: Wallet[] | any = await this.walletDbActions.getAllWallets();
+    console.log('wallets in db: ', wallets);
+
+    //extract wallet ids
+    const walletIds = wallets.map((wallet) => wallet.wId);
+
+    //fetch latest Transaction details for all wallets
+    const result = await this.fetchService.fetchWalletsLatestTxn(walletIds);
+
+    const walletsLatestTxnData: Record<
+      string,
+      {
+        collectionId: string;
+        transactionId: string;
+        From: string;
+        To: string;
+        TimeStamp: string;
+      }
+    > = result.walletsLatestTxnData;
+
+    const walletsResponses: Record<string, any> = result.walletsResponses;
+
+    //get list of wallets that have new activity
+    const deltaWallets: string[] | any = await this.deltaService.getDeltaWalletsList(
+      wallets,
+      walletsLatestTxnData,
+    );
+
+    //get delta transactions for wallets with new activity
+    const deltaTransactions = await this.deltaService.calcDelta(
+      deltaWallets,
+      wallets,
+      walletsLatestTxnData,
+      walletsResponses,
+    );
   }
 }

0577f84c24b017acbe933b6d8f214073f8453096
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-21 15:19:26 +0530

    config: refractor:

--- src/wallet-alerts/wallet-alerts.module.ts
+++ src/wallet-alerts/wallet-alerts.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CronService } from './alert-services/cron.service';
 import { PrismaDbModule } from 'src/prisma-db/prisma-db.module';
+import { HttpModule } from '@nestjs/axios';
+import { FetchService } from './simple-hash-services/fetch.service';
 
 @Module({
-  imports: [PrismaDbModule],
-  providers: [CronService],
+  imports: [PrismaDbModule, HttpModule],
+  providers: [CronService, FetchService],
   exports: [CronService],
 })
 export class WalletAlertsModule {}

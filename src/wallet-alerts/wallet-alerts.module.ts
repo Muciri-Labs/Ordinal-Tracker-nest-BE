8894789e1e0c17aa457fe0fca5bd8705744b6a5c
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-01 22:44:02 +0530

    feat: integrate

--- src/wallet-alerts/wallet-alerts.module.ts
+++ src/wallet-alerts/wallet-alerts.module.ts
@@ -4,10 +4,11 @@ import { PrismaDbModule } from 'src/prisma-db/prisma-db.module';
 import { HttpModule } from '@nestjs/axios';
 import { FetchService } from './simple-hash-services/fetch.service';
 import { DeltaService } from './alert-services/delta.service';
+import { TelegramService } from './telegram-services/send-alerts.service';
 
 @Module({
   imports: [PrismaDbModule, HttpModule],
-  providers: [CronService, FetchService, DeltaService],
+  providers: [CronService, FetchService, TelegramService, DeltaService],
   exports: [CronService],
 })
-export class WalletAlertsModule {}
+export class WalletAlertsModule { }

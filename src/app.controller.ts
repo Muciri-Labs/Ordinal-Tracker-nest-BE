2f5cc016b4b4840fe6017cf7cdf45f8c6929ece3
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-04-26 12:27:33 +0530

    init: feat: V2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { WalletDbService } from './prisma-db/wallets/wallet.service';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly walletService: WalletDbService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @Post('/wallet')
-  async addWallet(@Body() body: any) {
-    const { walletId } = body;
-    return this.walletService.addWallet(walletId);
-  }
-  @Get('/status')
-  async getStatus() {
-    return "Bot is active";
-  }
-
-  @Get('/wallet')
-  async getWallet(@Body() body: any) {
-    const { walletId } = body;
-    return this.walletService.getWallet(walletId);
-  }
-
-  @Post('/user-wallet')
-  async addUserWallet(@Body() body: any) {
-    const { userId, walletId, alertsEnabled } = body;
-    return this.walletService.addUserWallet(userId, walletId, alertsEnabled);
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PrismaDbModule } from './prisma-db/prisma-db.module';
-import { ScheduleModule } from '@nestjs/schedule';
-import { WalletAlertsModule } from './wallet-alerts/wallet-alerts.module';
-import { FloorAlertsModule } from './floor-alerts/floor-alerts.module';
-import { CaptureTelegramService } from './cpature-teleId.service';
+import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+
 @Module({
-  imports: [PrismaDbModule, ScheduleModule.forRoot(), WalletAlertsModule, FloorAlertsModule],
-  providers: [CaptureTelegramService],
-  // imports: [PrismaDbModule, ScheduleModule.forRoot()],
+  imports: [DbModule],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
-
-
-

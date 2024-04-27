c64e88e9734846915f6f87c906673062c1cfcd3c
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-04-27 15:55:17 +0530

    config:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../db/user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [UserModule],
+    imports: [UserModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '30d' },
+        })
+    ],
     controllers: [AuthController],
     providers: [AuthService, LocalStrategy]
 })

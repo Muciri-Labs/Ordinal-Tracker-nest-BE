8fb3bf9201724ee1427f55364f040e152676ea41
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-04-28 15:27:25 +0530

    fix: resolve dependency unavailable issue; create factory provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../db/user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './strategies/google.strategy';
+
+const GoogleStrategyProvider: Provider = {
+    provide: 'PassportStrategy',
+    useFactory: (authService: AuthService) => new GoogleStrategy(authService),
+    inject: [AuthService],
+};
 
 @Module({
     imports: [UserModule,
@@ -13,6 +20,6 @@ import { JwtModule } from '@nestjs/jwt';
         })
     ],
     controllers: [AuthController],
-    providers: [AuthService, LocalStrategy]
+    providers: [AuthService, LocalStrategy, GoogleStrategyProvider]
 })
 export class AuthModule { }

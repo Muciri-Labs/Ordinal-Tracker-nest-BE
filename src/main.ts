cc37cd9ed0373b69e66d53dfd801bfffdb793ed4
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-05-09 13:08:42 +0530

    deploy:

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['http://localhost:3001', 'https://ordinal-tracker.onrender.com/dashboard'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();

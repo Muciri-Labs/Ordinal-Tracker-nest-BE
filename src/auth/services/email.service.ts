6c36b0662fd17f69a5ba53894dc683b8c58705e4
Hurmaan Ahmed <contactme.hurmaan@gmail.com>
2024-05-27 17:00:07 +0530

    feat: create email service

---
+++ src/auth/services/email.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { Resend } from 'resend';
+import { TemplateService } from './template.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class EmailService {
+  private resend: Resend;
+
+  constructor(
+    private readonly templateService: TemplateService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.resend = new Resend(process.env.RESEND_API_KEY);
+  }
+
+  async sendEmailVerification(email: string) {
+    const token = this.jwtService.sign({
+      type: 'verify-token',
+      email,
+    });
+
+    const html = this.templateService.getOnboardingTemplate(token);
+
+    const { data, error } = await this.resend.emails.send({
+      from: 'Storm Glass ⚡ <onboarding@stormglass.xyz>',
+      to: [email],
+      subject: 'Confirm your email address',
+      html: html,
+    });
+
+    if (error) {
+      return console.error({ error });
+    }
+
+    console.log({ data });
+  }
+}
